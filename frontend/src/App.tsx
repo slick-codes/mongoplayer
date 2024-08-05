1376aca29c46094b55bab78c7184d2ebb182f89b
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-05 16:07:41 +0100

    remove scalford and start working on navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,15 @@
-import { useState } from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import { Greet } from "../wailsjs/go/main/App";
-import { EventsEmit, EventsOn, WindowFullscreen } from "./../wailsjs/runtime/runtime"
+import Main from "./components/Main"
 
 function App() {
 
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    const [index, setIndex] = useState(0)
-
-    function greet() {
-        // Greet(name).then(updateResultText);
-        EventsEmit("testing", { name: "Paul" })
-        EventsOn("increment", function(index) {
-            setIndex(index)
-        })
-    }
-
-
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo" />
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text" />
-                <button className="btn" onClick={greet}>{index} Greet</button>
+        <>
+            <div className="mongoplayer-main-container">
+                <Main />
             </div>
-        </div>
+        </>
     )
 }
+
 
 export default App
