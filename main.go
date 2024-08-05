50521e0ccd4d420f212771658b657b104cf22230
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-05 15:36:33 +0100

    update the main file to match the changes done on other files

--- main.go
+++ main.go
@@ -6,29 +6,36 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-    // "github.com/wailsapp/wails/v2/pkg/runtime"
-    )
-
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	// "github.com/wailsapp/wails/v2/pkg/runtime"
+)
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-    app := NewApp()
+	app := NewApp()
 
-
-    	// Create application with options
+	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "mongoplayer",
-		Width:  1024,
-		Height: 768,
-        Fullscreen: false,
+		Title:      "mongoplayer",
+		Width:      1024,
+		Height:     768,
+		Fullscreen: false,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+
+		// Linux configuration
+		Linux: &linux.Options{
+			WindowIsTranslucent: false,
+		},
+
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		OnDomReady:       app.domReady,
+		Debug:            options.Debug{OpenInspectorOnStartup: true},
 		Bind: []interface{}{
 			app,
 		},
