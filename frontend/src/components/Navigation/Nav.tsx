79c6247c65b424c2d01616dede47fdff91e25914
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-06 13:52:38 +0100

    fix the functionality of the Nav toggler

--- frontend/src/components/Navigation/Nav.tsx
+++ frontend/src/components/Navigation/Nav.tsx
@@ -15,13 +15,13 @@ import Settings from "../icons/Settings";
 
 
 interface Directory {
-    name: String,
-    dir: String,
+    name: string,
+    dir: string,
     icon: React.ComponentType | string,
 }
 
 interface Playlist {
-    name: String;
+    name: string;
     icon: React.ComponentType | string
 }
 
@@ -38,14 +38,32 @@ export default function() {
         { name: "Favorite", icon: Heart },
     ]
 
+    // toggle navigation handlers
+    const [navIsOpen, setNavState] = React.useState(true)
+    const toggleNavState = () => setNavState(state => !state)
+
+    const openFolder = function() {
+        const input = document.createElement("input")
+        input.type = "file"
+        input.setAttribute("webkitdirectory", "")
+        input.setAttribute("directory", "")
+        input.setAttribute("multiple", "")
+        console.log(input)
+        input.click()
+    }
+
+
+
     return (
         <>
-            <aside className="nav">
+            <aside className={`nav ${!navIsOpen && "--closed"}`}>
                 <div className="nav__container">
                     <div className="nav__content">
 
                         <section className="nav__toggler">
-                            <NavBar />
+                            <div onClick={toggleNavState}>
+                                <NavBar />
+                            </div>
                         </section>
 
                         <section className="nav__directory_container">
@@ -55,14 +73,17 @@ export default function() {
                                         return (
                                             <>
                                                 <li>
-                                                    <span key={key}>{directory.icon && <directory.icon />}</span>
+                                                    <span
+                                                        title={!navIsOpen ? directory.name : undefined} key={key}>
+                                                        {directory.icon && <directory.icon />}
+                                                    </span>
                                                     <span>{directory.name}</span>
                                                 </li>
                                             </>
                                         )
                                     })}
                                     <li>
-                                        <button>Add</button>
+                                        <button onClick={openFolder}>Add</button>
                                     </li>
                                 </div>
                             </DropDown>
@@ -75,7 +96,7 @@ export default function() {
                                         return (
                                             <>
                                                 <li>
-                                                    <span key={key}>{item.icon && <item.icon />}</span>
+                                                    <span title={!navIsOpen ? item.name : undefined} key={key}>{item.icon && <item.icon />}</span>
                                                     <span>{item.name}</span>
                                                 </li>
                                             </>
