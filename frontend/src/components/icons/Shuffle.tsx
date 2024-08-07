797746320ca2d5ce3b3e2c2831fddb77df5ad730
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-08 00:01:17 +0100

    add icon components

---
+++ frontend/src/components/icons/Shuffle.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+function Repeat() {
+    return (
+
+        <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.777954 5.81909V5.31909C0.777954 4.78866 0.988668 4.27995 1.36374 3.90488C1.73881 3.52981 2.24752 3.31909 2.77795 3.31909H9.77795M2.77795 11.3191L0.777954 9.31909L2.77795 7.31909" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M7.77795 1.31909L9.77795 3.31909L7.77795 5.31909" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M9.77795 6.81909V7.31909C9.77795 7.84952 9.56724 8.35823 9.19217 8.7333C8.81709 9.10838 8.30839 9.31909 7.77795 9.31909H0.777954M4.77795 5.31909H5.27795V7.31909" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
+
+
+export default Repeat
