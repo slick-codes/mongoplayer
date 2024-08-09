a61cdd4c9223d6775682094c4df101abdb0c522f
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-09 13:49:50 +0100

    set the Panel layouts

--- frontend/src/components/Panel/Panel.tsx
+++ frontend/src/components/Panel/Panel.tsx
@@ -1,12 +1,19 @@
 
 
+import Control from "../AudioControl/Control"
 import "./../../styles/panel.scss"
 
 
 function Panel() {
     return (
         <>
-            <section className="panel"></section>
+            <section className="panel">
+                <div className="panel__container">
+                    <section>Header</section>
+                    <section>List</section>
+                    <Control />
+                </div>
+            </section>
         </>
     )
 }
