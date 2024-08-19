17129f03ecc5512688aacf7c8099e1689afdf6f6
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-08-19 13:11:45 +0100

    create the List component to hold audio list

---
+++ frontend/src/components/Panel/AudiosPanel/List.tsx
@@ -0,0 +1,416 @@
+
+
+import VerticalMenu from "../../icons/VerticalMenu"
+import "./../../../styles/list.scss"
+
+
+
+const List: React.FC = function() {
+
+    const audios = [
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        },
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        },
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+
+
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+        ,
+        {
+            FileDir: "",
+            Album: "www.naijasermans.com.ng",
+            nameInDir: "",
+            Artist: "somebody like that",
+            FileName: "",
+            Genre: "",
+            Title: "Fragrance To fire || www.naijasermans.com.ng",
+            Lyrics: "",
+            AlbumAirtist: "",
+            Year: ""
+        }
+
+    ]
+
+    return (<>
+        <section className="list">
+            <div className="overflow"></div>
+            <div className="list__container">
+                <header className="list__header">
+                    <div>Title</div>
+                    <div>Artist</div>
+                    <div>Album</div>
+                    <div>Added</div>
+                    <div>Duration</div>
+                    <div></div>
+                </header>
+                <div className="list__content">
+                    <div className="list__item__container">
+                        {audios.map((audio, index) => (
+                            <div className="audio__item">
+                                <>
+                                    <div>{audio.Title || audio.FileName}</div>
+                                    <div>{audio.Artist}</div>
+                                    <div>{audio.Album}</div>
+                                    <div>2 weeks ago</div>
+                                    <div>06:54</div>
+                                    <div>
+                                        <button>
+                                            <VerticalMenu />
+                                        </button>
+                                    </div>
+                                </>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>)
+}
+
+
+export default List
